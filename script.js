f614642246a19cfc08e4572a7ec9fd84b1dfefb4
krupa-ad <adhikrupa992@gmail.com>
2025-06-03 17:34:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select the button by its ID
+const button = document.getElementById('alertBtn');
+
+// Add a click event listener
+button.addEventListener('click', function() {
+    alert('Hello! Thanks for clicking the button!');
+});
